ced63dda9211aff12eb243fb7a543e37ed69d311
dmukhopadov <d.mukhopadov@futuro.team>
2021-02-18 16:58:53 +0200

    :tada: Initial commit.

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res, nexxt)=>{
+    console.log('invalid connection')
+    nexxt()
+})
+
+app.use('/callback', (req, res, next)=>{
+    const {method, ip, originalUrl, params} = req;
+    console.log(JSON.stringify({
+        method,
+        ip,
+        originalUrl
+    }))
+    console.log('end connection');
+    res.status(200).send('test callback')
+})
+
+app.listen(5000, ()=>{
+    console.log('server running')
+})
